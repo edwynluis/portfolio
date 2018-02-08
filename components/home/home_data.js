599acf8d8d611a133815aef38739ef1fa48849c1
Unknown <edwynluis@yahoo.com.br>
2018-02-08 10:03:09 +0100

    data file

---
+++ components/home/home_data.js
@@ -0,0 +1,68 @@
+$scope.categories = [
+	{
+		title : "Artificial Intelligence"
+		items : [
+			{
+				title 			: "A star / Dijkstra",
+				short_desc		: "",
+				full_desc 		: "",
+				keypoints 		: "",
+				tools	 		: "",
+				repository		: "https://github.com/Edwyn-Gamagora2017",
+				play			: undefined,
+				preview_image 	: { img : "../assets/img/projects/astar_sample.png", alt : "Astar" },
+				images 			: [
+									{ img : "../assets/img/projects/astar_sample.png", alt : "Astar" }
+								],
+				videos			: [
+									{ img : "../assets/img/projects/astar_sample.png", link : "https://github.com/Edwyn-Gamagora2017", alt : "Astar" }
+								]
+			}
+		]
+	}
+];
+/*{
+	title 			: "",
+	short_desc		: "",
+	full_desc 		: "",
+	keypoints 		: "",
+	tools	 		: "",
+	repository		: undefined,
+	play			: undefined,
+	preview_image 	: { img : "../assets/img/projects/", alt : "" },
+	images 			: [
+						{ img : "../assets/img/projects/", alt : "" }
+					],
+	videos			: [
+						{ img : "", link : "", alt : "" }
+					]
+}*/
+
+/*
+
+- Games
+== Zombie Escape
+== Billy
+== Vision
+
+- Education
+== Desafio
+== UCC
+
+- AI
+== A*
+== Flocking
+== Genetic
+
+- General
+Shading
+Geracao de imagens
+fisica; cinematica e fluido
+
+Modelisacao: objetos
+Modelisacao: malhas
+Modelisacao: superficies
+Modelisacao: volume
+Modelisacao: simplificacao/subdivisao; geracao de terreno e rotas
+
+*/
